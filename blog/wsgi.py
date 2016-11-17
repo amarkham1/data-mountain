943348d436805d92171ee98da752d4c38de093d7
amarkham <andrewjmarkham1@gmail.com>
2016-11-16 22:20:20 -0500

    Heroku

--- blog/wsgi.py
+++ blog/wsgi.py
@@ -11,6 +11,7 @@ import os
 import sys
 
 sys.stdout = sys.stderr
+import django.core.wsgi
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.settings")
