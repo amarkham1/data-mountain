9489e9e1e77e0ad5d68cd0fa94b73f6c7764ef22
amarkham <andrewjmarkham1@gmail.com>
2016-11-16 20:43:14 -0500

    Heroku

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/amarkham/data-mountain/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- venv/bin/pwhich.py
+++
@@ -1,9 +0,0 @@
-#!/Users/amarkham/data-mountain/venv/bin/python
-import os
-import sys
-imported_module = __import__(sys.argv[1])
-try:
-    print os.path.abspath(os.path.dirname(imported_module.__file__))
-except AttributeError:
-    print 'stdlib'
-
