88a0780e250d22aca17df9e9946a926e73282714
amarkham <andrewjmarkham1@gmail.com>
2016-11-17 20:50:17 -0500

    Heroku

--- wsgi.py
+++ wsgi.py
@@ -10,14 +10,15 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 import sys
 
-sys.stdout = sys.stderr
-import django.core.wsgi
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.settings")
 
-
-from dj_static import Cling
-application = Cling(get_wsgi_application())
+sys.stdout = sys.stderr
+from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
+
+
+
+
