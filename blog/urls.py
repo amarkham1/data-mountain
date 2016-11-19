d9b8bd216ce108d7cb18425727e8a9abefaa7f52
amarkham <andrewjmarkham1@gmail.com>
2016-11-19 10:51:49 -0500

    Herokue v2

--- blog/urls.py
+++ blog/urls.py
@@ -14,8 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-
-#from django.conf.urls import patterns, include, url
 from django.contrib import admin
 import django.contrib.auth.views
 from books import views
