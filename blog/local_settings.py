74b0ff49103a735e0c9fa1f14c2cf951c5975060
amarkham <andrewjmarkham1@gmail.com>
2016-11-27 13:15:59 -0500

    Heroku

--- blog/local_settings.py
+++ blog/local_settings.py
@@ -2,7 +2,8 @@ from settings import *
 import os
 import dj_database_url
 
-DEBUG = TEMPLATE_DEBUG = False
+DEBUG = False
+TEMPLATE_DEBUG = True
 
 SECRET_KEY='1c3-cr3am-b4by'
 ALLOWED_HOSTS = ['www.datamountain.ca']
