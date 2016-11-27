8ab2de2133cbe20489a0330ad62a60afa80b36a3
amarkham <andrewjmarkham1@gmail.com>
2016-11-27 12:47:16 -0500

    Heroku

--- blog/local_settings.py
+++ blog/local_settings.py
@@ -13,7 +13,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 #    'default': dj_database_url.config(default='postgres://amarkham:Amarkham1@localhost:5432/blogdb')
 #}
 
-
+ADMINS = (
+    ('Andrew', 'andrewjmarkham1@gmail.com'),
+)
 
 DATABASES = {
     'default': {
