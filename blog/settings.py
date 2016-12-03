ce2d7e38637526a4195e368ce543a889dc502ade
amarkham <andrewjmarkham1@gmail.com>
2016-12-03 18:21:44 -0500

    Heroku

--- blog/settings.py
+++ blog/settings.py
@@ -128,13 +128,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_SSL_REDIRECT = False # set to False when in development
+#SECURE_SSL_REDIRECT = False # set to False when in development
 #SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_BROWSER_XSS_FILTER = True
-SESSION_COOKIE_SECURE = True
-X_FRAME_OPTIONS = 'DENY'
-CSRF_COOKIE_HTTPONLY = True
-CSRF_COOKIE_SECURE = True
+#SESSION_COOKIE_SECURE = True
+#X_FRAME_OPTIONS = 'DENY'
+#CSRF_COOKIE_HTTPONLY = True
+#CSRF_COOKIE_SECURE = True
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
